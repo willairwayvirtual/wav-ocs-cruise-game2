66be47443a1f26c505d727b19147fda88df0402f
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-04-21 14:19:40 +0100

    adgsl/jhyrflyyyrenlk
    gshottrkjk vdjkifngp b kg

--- Assets/Script/menu3.cs
+++ Assets/Script/menu3.cs
@@ -2,23 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
-public class menu2 : MonoBehaviour
+public class menu3 : MonoBehaviour
 {
     public void playgame()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+
     }
-    public void playgame1()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
-    }
-   
+
+
     public void Quit()
     {
         Debug.Log("quit?");
         Application.Quit();
     }
-
 }
 

2154be431ae93ed1be169069890a97f37cd4b8b4
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-05-02 16:04:40 +0100

    nhfplhbcgo;.lv kongfcbvjihncgbkjnhkio
    wdkjrefdicxukohtcgujigdcofvijfesdxbscvxpbcnhfblnv lmhbv knhfvklmhnvkl,xfv[lvflpng;klnhlmhlmh

---
+++ Assets/Script/Options.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+
+public class Options : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public Dropdown resolutionDropdown;
+
+    Resolution[] resolutions;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions;
+
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+        resolutionDropdown.RefreshShownValue();
+    }
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+    public void setvolume(float volume)
+    {
+        audioMixer.SetFloat("wind", volume);
+    }
+    public void SetQuality (int qualityIndex) 
+    { 
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+    public void SetFullScreen (bool isFullScreen)
+    {
+    Screen.fullScreen = isFullScreen;
+    }
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+
+    }
+}
+
+
+
+

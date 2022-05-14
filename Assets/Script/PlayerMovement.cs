3d1c1f9f4d2a749eed58c4050d4e6b028e3084ed
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-05-14 12:09:49 +0100

    dhplbfxvokfdl;rkospf
    hfdovlchgnfvikpbgdkcxzgdkogsedxckdv

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -15,22 +15,27 @@ public class PlayerMovement : MonoBehaviour
 	// are using it to mess with physics.
 	void FixedUpdate()
 	{// Add a forward force
-		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+		//rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 		// Add a forward force
-		rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+		//rb.AddForce(0, 0, forwardForce * Time.deltaTime);
 
-		if (Input.GetKey("d"))  // If the player is pressing the "d" key
+		if (Input.GetKey("w"))  // If the player is pressing the "w" key
 		{
-			// Add a force to the right
+			// Add a force to go forward
+			rb.AddForce(0,0, forwardForce * Time.deltaTime);
+		}
+		  
+		if (Input.GetKey("s"))  // If the player is pressing the "s" key
+		{
+			// Add a force to go back
+			rb.AddForce(0,0, -forwardForce * Time.deltaTime);
+		}
+		if (Input.GetKey("d"))  // If the player is pressing the "d" key
+		{        // Add a force to the right
 			rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
 		}
 
 		if (Input.GetKey("a"))  // If the player is pressing the "a" key
-		{
-			// Add a force to the left
-			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
-		}
-		if (Input.GetKey("a"))  // If the player is pressing the "d" key
 		{        // Add a force to the left
 			rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
 		}

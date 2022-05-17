53e184e5b29024a1f57c153bbf6dadf174d15bcf
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-05-17 14:35:05 +0100

    paus
    kadlfplhltdfvokjigrdofiinhogfbvikhtdgfpvkhtgfblvcdbgdbkbtkogfokgrkgrfdp[hdttpkoe[dfbcvl

---
+++ Assets/Script/paus.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paus : MonoBehaviour
+{
+    public static bool gameispaused = false;
+
+    public GameObject PauseMenuUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+                if (gameispaused)
+
+                {
+                    Resume();
+                }
+                else
+                {
+                    pause();
+                }
+        }
+    }
+    void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameispaused = false;
+    }
+    void pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameispaused = true;
+    }
+}
+

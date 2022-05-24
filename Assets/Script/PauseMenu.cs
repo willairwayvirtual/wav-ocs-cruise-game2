61a600e1bf6cad46181a748e430c58d6ceb136cf
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2022-05-24 10:13:32 +0100

    qeethlkg ;rekikjw,f
    2ugfwsrtu8ikrwmtgr,

--- Assets/Script/PauseMenu.cs
+++ Assets/Script/PauseMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class paus : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
     public static bool gameispaused = false;
 

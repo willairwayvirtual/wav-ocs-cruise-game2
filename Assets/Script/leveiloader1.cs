aa2b0f5b34a12c250e9ac3d3a97bf7fee956d778
willairwayvirtual <40953328+willairwayvirtual@users.noreply.github.com>
2023-01-06 09:05:28 +0000

    dw

---
+++ Assets/Script/leveiloader1.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class leveiloader1 : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+    public Text progressText;
+  public void LoadLevel(int sceneIndex)
+    {
+        StartCoroutine(LoadAsynchronously(sceneIndex));
+    }
+       
+    IEnumerator LoadAsynchronously (int sceneIndex)
+
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        loadingScreen.SetActive(true);
+        while (!operation.isDone)
+            {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+
+}
---
+++ Assets/Script/levelmenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class levelmenu : MonoBehaviour
+{
+
+    public void Quit()
+    {
+        Debug.Log("quit?");
+        Application.Quit();
+    }
+    public void restart1()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 3);
+    }
+
+
+    public void restart21()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 2);
+    }
+
+}
+
